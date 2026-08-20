fb69283a3babff4a5293b8bf7d21310881a0a2c7
utsavsahu12345 <utsavsahu12345@gmail.com>
2026-08-20 23:04:42 +0530

    dfj

--- src/router.tsx
+++ src/router.tsx
@@ -10,6 +10,7 @@ export const getRouter = () => {
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+    basepath: "/KGN-MANDI-HOUSE/",
   });
 
   return router;
